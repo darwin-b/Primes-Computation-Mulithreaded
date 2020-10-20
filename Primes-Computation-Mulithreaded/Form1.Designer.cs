ef7ebd6b0b556d66138638a3303443533b0f398e
darwin_b <darwin.bollepalli@gmail.com>
2020-10-21 02:24:00 +0530

    Added Computaion Tasks and fixed bugs in multi threading.

--- Primes-Computation-Mulithreaded/Form1.Designer.cs
+++ Primes-Computation-Mulithreaded/Form1.Designer.cs
@@ -29,25 +29,28 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.lblFactorsProgress = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.lstViewResults = new System.Windows.Forms.ListView();
+            this.colNumber = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colFactors = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.btnCompute = new System.Windows.Forms.Button();
             this.msktxtLowerBound = new System.Windows.Forms.MaskedTextBox();
             this.msktxtUpperBound = new System.Windows.Forms.MaskedTextBox();
             this.lblLowerBound = new System.Windows.Forms.Label();
             this.lblUpperBound = new System.Windows.Forms.Label();
-            this.lstViewResults = new System.Windows.Forms.ListView();
-            this.colNumber = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.colFactors = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.lblMsg = new System.Windows.Forms.Label();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.lblProgress = new System.Windows.Forms.Label();
-            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.lblProgress);
+            this.panel1.Controls.Add(this.progressBar2);
+            this.panel1.Controls.Add(this.lblFactorsProgress);
             this.panel1.Controls.Add(this.progressBar1);
             this.panel1.Controls.Add(this.lblMsg);
             this.panel1.Controls.Add(this.lstViewResults);
@@ -58,8 +61,72 @@
             this.panel1.Controls.Add(this.lblUpperBound);
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(612, 696);
+            this.panel1.Size = new System.Drawing.Size(666, 725);
             this.panel1.TabIndex = 0;
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.AutoSize = true;
+            this.lblProgress.Location = new System.Drawing.Point(33, 343);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(48, 13);
+            this.lblProgress.TabIndex = 14;
+            this.lblProgress.Text = "Progress";
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(36, 441);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(189, 15);
+            this.progressBar2.TabIndex = 13;
+            // 
+            // lblFactorsProgress
+            // 
+            this.lblFactorsProgress.AutoSize = true;
+            this.lblFactorsProgress.Location = new System.Drawing.Point(33, 425);
+            this.lblFactorsProgress.Name = "lblFactorsProgress";
+            this.lblFactorsProgress.Size = new System.Drawing.Size(86, 13);
+            this.lblFactorsProgress.TabIndex = 12;
+            this.lblFactorsProgress.Text = "Factors Progress";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(36, 359);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(189, 15);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Location = new System.Drawing.Point(33, 278);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(37, 13);
+            this.lblMsg.TabIndex = 8;
+            this.lblMsg.Text = "Status";
+            // 
+            // lstViewResults
+            // 
+            this.lstViewResults.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colNumber,
+            this.colFactors});
+            this.lstViewResults.HideSelection = false;
+            this.lstViewResults.Location = new System.Drawing.Point(332, 73);
+            this.lstViewResults.Name = "lstViewResults";
+            this.lstViewResults.Size = new System.Drawing.Size(308, 564);
+            this.lstViewResults.TabIndex = 7;
+            this.lstViewResults.UseCompatibleStateImageBehavior = false;
+            this.lstViewResults.View = System.Windows.Forms.View.Details;
+            // 
+            // colNumber
+            // 
+            this.colNumber.Text = "Number";
+            this.colNumber.Width = 86;
+            // 
+            // colFactors
+            // 
+            this.colFactors.Text = "Factors";
+            this.colFactors.Width = 203;
             // 
             // btnCompute
             // 
@@ -76,7 +143,7 @@
             this.msktxtLowerBound.Location = new System.Drawing.Point(109, 109);
             this.msktxtLowerBound.Mask = "000000000000000000";
             this.msktxtLowerBound.Name = "msktxtLowerBound";
-            this.msktxtLowerBound.Size = new System.Drawing.Size(128, 20);
+            this.msktxtLowerBound.Size = new System.Drawing.Size(116, 20);
             this.msktxtLowerBound.TabIndex = 5;
             // 
             // msktxtUpperBound
@@ -84,7 +151,7 @@
             this.msktxtUpperBound.Location = new System.Drawing.Point(109, 73);
             this.msktxtUpperBound.Mask = "000000000000000000";
             this.msktxtUpperBound.Name = "msktxtUpperBound";
-            this.msktxtUpperBound.Size = new System.Drawing.Size(128, 20);
+            this.msktxtUpperBound.Size = new System.Drawing.Size(116, 20);
             this.msktxtUpperBound.TabIndex = 4;
             // 
             // lblLowerBound
@@ -105,75 +172,22 @@
             this.lblUpperBound.TabIndex = 0;
             this.lblUpperBound.Text = "Upper Bound";
             // 
-            // lstViewResults
-            // 
-            this.lstViewResults.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.colNumber,
-            this.colFactors});
-            this.lstViewResults.HideSelection = false;
-            this.lstViewResults.Location = new System.Drawing.Point(278, 73);
-            this.lstViewResults.Name = "lstViewResults";
-            this.lstViewResults.Size = new System.Drawing.Size(316, 562);
-            this.lstViewResults.TabIndex = 7;
-            this.lstViewResults.UseCompatibleStateImageBehavior = false;
-            this.lstViewResults.View = System.Windows.Forms.View.Details;
-            // 
-            // colNumber
-            // 
-            this.colNumber.Text = "Number";
-            this.colNumber.Width = 86;
-            // 
-            // colFactors
-            // 
-            this.colFactors.Text = "Factors";
-            this.colFactors.Width = 203;
-            // 
             // backgroundWorker1
             // 
             this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgWorkerDoWork_Compute);
             this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
             this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
             // 
-            // lblMsg
-            // 
-            this.lblMsg.AutoSize = true;
-            this.lblMsg.Location = new System.Drawing.Point(36, 258);
-            this.lblMsg.Name = "lblMsg";
-            this.lblMsg.Size = new System.Drawing.Size(35, 13);
-            this.lblMsg.TabIndex = 8;
-            this.lblMsg.Text = "status";
-            // 
-            // progressBar1
-            // 
-            this.progressBar1.Location = new System.Drawing.Point(36, 359);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(100, 23);
-            this.progressBar1.TabIndex = 9;
-            // 
-            // lblProgress
-            // 
-            this.lblProgress.AutoSize = true;
-            this.lblProgress.Location = new System.Drawing.Point(33, 310);
-            this.lblProgress.Name = "lblProgress";
-            this.lblProgress.Size = new System.Drawing.Size(48, 13);
-            this.lblProgress.TabIndex = 10;
-            this.lblProgress.Text = "Progress";
-            // 
-            // backgroundWorker2
-            // 
-            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
-            this.backgroundWorker2.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker2_ProgressChanged);
-            this.backgroundWorker2.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker2_RunWorkerCompleted);
-            // 
             // Form1
             // 
+            this.AcceptButton = this.btnCompute;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(641, 720);
+            this.ClientSize = new System.Drawing.Size(690, 749);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MultiThreaded Computation";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -194,8 +208,9 @@
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.Label lblMsg;
         private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Label lblFactorsProgress;
         private System.Windows.Forms.Label lblProgress;
-        private System.ComponentModel.BackgroundWorker backgroundWorker2;
     }
 }
 
--- Primes-Computation-Mulithreaded/Form1.cs
+++ Primes-Computation-Mulithreaded/Form1.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿
+/******************************************************************************
+* Multithreaded Computation
+*
+* This program takes lower & upper bound values from fields as input and does 2 computation 
+* tasks which are run as separate threads from that of main user interface thread.
+* 
+* Computation Tasks
+*   - Compute and store all primes less than or equal to the square root of the upper bound.  
+*   - Compute and display the numbers in the range of lower & upper bounds, along with their unique prime factors.  
+*   
+*	
+* Written by Nikhil Darwin Bollepalli, starting October 14, 2020.
+******************************************************************************/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,13 +27,17 @@ namespace Primes_Computation_Mulithreaded
     public partial class Form1 : Form
     {
 
-        List<ListViewItem> listResults = new List<ListViewItem>();
+        System.Drawing.Color colorGreen = System.Drawing.Color.Green;
+        System.Drawing.Color colorRed = System.Drawing.Color.Red;
+        System.Drawing.Color colorBlack = System.Drawing.Color.Black;
         HashSet<int> hashSetPrimes = new HashSet<int>();
         public Form1()
         {
             InitializeComponent();
+            InitializeBackgroundWorker();
             backgroundWorker1.WorkerReportsProgress = true;
             backgroundWorker1.WorkerSupportsCancellation = true;
+
             //lstViewResults.Items.Add(primes_compute(2310));
         }
 
@@ -27,144 +45,245 @@ namespace Primes_Computation_Mulithreaded
         {
             if (backgroundWorker1.IsBusy && backgroundWorker1.WorkerSupportsCancellation == true)
             {
-                lblMsg.Text = "Bg busy and cancelling";
-                listResults = new List<ListViewItem>();
+                lblMsg.Text = "BGWorker busy and cancelling";
                 lstViewResults.Items.Clear();
                 backgroundWorker1.CancelAsync();
+                lblFactorsProgress.Visible = true;
+                lblProgress.Visible = true;
+                lblProgress.Text = "Computation1";
+                lblFactorsProgress.Text = "Computation2";
+                lblMsg.ForeColor = colorBlack;
+                progressBar1.Value = 0;
+                progressBar2.Value = 0;
                 //backgroundWorker1.RunWorkerAsync(1000);
             }
             else
             {
-                lblMsg.Text = "BG not busy - starting worker";
+                lblMsg.Text = "BGWorker not busy - starting worker";
                 lstViewResults.Items.Clear();
-                backgroundWorker1.CancelAsync();
-                lblProgress.Text = "";
-                progressBar1.Value = 0;
-                backgroundWorker1.RunWorkerAsync(1000);
+                try
+                {
+                    int lBound = int.Parse(msktxtLowerBound.Text.Replace(" ", ""));
+                    int uBound = int.Parse(msktxtUpperBound.Text.Replace(" ", ""));
+                    lblFactorsProgress.Visible = true;
+                    lblProgress.Visible = true;
+                    lblProgress.Text = "Computation1";
+                    lblFactorsProgress.Text = "Computation2";
+                    lblMsg.Text = "";
+                    lblMsg.ForeColor = colorBlack;
+                    progressBar1.Value = 0;
+                    progressBar2.Value = 0;
+                    List<int> args = new List<int>();
+                    args.Add(lBound);
+                    args.Add(uBound);
+                    backgroundWorker1.RunWorkerAsync(args);
+                }
+                catch(Exception exception)
+                {
+                    lblMsg.Text = "Error : Empty fields are not valid input";
+                    lblMsg.ForeColor = colorRed;
+                }
+                   
+
+
+                
             }
 
-            hashSetPrimes = primes(10000);
-            
+            //hashSetPrimes = primes(10000);
+
         }
 
 
-        private void Form1_Load(object sender, EventArgs e)
+        /************************************************************************************
+         * InitializeBackgroundWorker() : Initializes BachgroundWorker with event handlers.   
+         * 
+         ***********************************************************************************/
+        private void InitializeBackgroundWorker()
         {
-
-            backgroundWorker1.DoWork += bgWorkerDoWork_Compute;
-
+            backgroundWorker1 = new BackgroundWorker();
+            backgroundWorker1.DoWork += new DoWorkEventHandler(bgWorkerDoWork_Compute);
+            backgroundWorker1.RunWorkerCompleted += new RunWorkerCompletedEventHandler(backgroundWorker1_RunWorkerCompleted);
+            backgroundWorker1.ProgressChanged += new ProgressChangedEventHandler(backgroundWorker1_ProgressChanged);
         }
 
+
+        /************************************************************************************
+         *  backgroundWorker1_RunWorkerCompleted() : handles and updates to UI main thread on success results or 
+         *                                          failure status of background thread.
+         * 
+         ***********************************************************************************/
         private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
-            if (e.Cancelled)
+
+            if (e.Error != null)
             {
-                lblProgress.Text = "Operation was canceled";
+                lblMsg.Text = "Error: " + e.Error.Message;
+                lblMsg.ForeColor = colorRed;
+            }
+            else if(e.Cancelled)
+            {
+                lblMsg.Text = "Operation was canceled and started re-computing";
+                lblMsg.ForeColor = colorRed;
                 btnCompute_Click(sender, e);
             }
-            else if (e.Error != null) lblProgress.Text = "Error: " + e.Error.Message;
             else
             {
-                lstViewResults.Items.Clear();
-                foreach (ListViewItem item in listResults)
-                {
-                    lstViewResults.Items.Add(item);
-                }
-                listResults = new List<ListViewItem>();
+                lblMsg.Text = "Computations finished!";
+                lblMsg.ForeColor = colorGreen;
             };
         }
 
+
+        /************************************************************************************
+         *  backgroundWorker1_ProgressChanged() : handles and updates to UI main thread on progress changes.
+         * 
+         ***********************************************************************************/
         private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
-            //if(typeof(e)=typeof(ListViewItem))
-            //{
+            //Console.WriteLine(typeof())
+            //var lstItemsFactors = (ListViewItem)e.UserState;
+            var lstItemsFactors = e.UserState as ListViewItem;
+            if (lstItemsFactors != null)
+            {
+                lblMsg.Text = "Factorising Number : " + lstItemsFactors.SubItems[0].Text;
+                lstViewResults.Items.Add(lstItemsFactors);
+                lblFactorsProgress.Text = ("Computation 2 : " + e.ProgressPercentage.ToString() + "%");
+                Console.WriteLine("Progress : " + lblProgress.Text + "%");
+                progressBar2.Value = int.Parse(e.ProgressPercentage.ToString());
+            }
+            else
+            {
+                lblMsg.Text = e.UserState.ToString();
+                lblProgress.Text = ("Computation 1: " + e.ProgressPercentage.ToString() + "%");
+                Console.WriteLine("Progress : " + lblProgress.Text + "%");
+                progressBar1.Value = int.Parse(e.ProgressPercentage.ToString());
+            }
 
-            //}
-            lblProgress.Text = (e.ProgressPercentage.ToString() + "%");
-            progressBar1.Value = int.Parse(e.ProgressPercentage.ToString());
+
         }
 
 
+        /************************************************************************************
+         *  primes_compute() : Computation2 - Compute and display the numbers in the range of lower & upper bounds,
+         *                                    along with their unique prime factors given primes upto square root of upperbound (computaion1 results)
+         * 
+         ***********************************************************************************/
         private ListViewItem primes_compute(int num, HashSet<int> primesSet)
         {
 
             ListViewItem listFactors = new ListViewItem(num.ToString());
-            int bound = (int) Math.Sqrt(num);
-            string factors="";
-            foreach(var prime in primesSet)
+            int bound = (int)Math.Sqrt(num);
+            string factors = "";
+            foreach (var prime in primesSet)
             {
-                if(num%prime==0) factors += prime.ToString()+" ";
+                if (num % prime == 0) factors += prime.ToString() + " ";
+                while (num % prime == 0) num = num / prime;
             }
 
+            if (num != 1) factors += num.ToString();
             listFactors.SubItems.Add(factors);
             //lstViewResults.Items.Add(number);
             return listFactors;
         }
 
-        private HashSet<int> primes(int upperBound)
+
+        /************************************************************************************
+         *  primes() : Computation1 - Compute and store all primes less than or equal to the square root of the upper bound in hashset
+         * 
+         ***********************************************************************************/
+        private HashSet<int> primes(int upperBound, BackgroundWorker worker, DoWorkEventArgs e)
         {
             HashSet<int> primes = new HashSet<int>();
-            int bound1 = (int) Math.Ceiling(Math.Sqrt(upperBound));
-            for (int i=2;i<=bound1;i++)
+            int bound1 = (int)Math.Ceiling(Math.Sqrt(upperBound));
+            for (int i = 2; i <= bound1; i++)
             {
-                bool flagP = true;
-                int bound2 = (int)Math.Ceiling(Math.Sqrt(upperBound));
-                for (int j=2;j<=bound2;j++)
-                {
-                    if(i%j==0 && i!=j)
-                    {
-                        flagP = false;
-                        break;
-                    }
-                }
-                if(flagP)                 
-                    primes.Add(i);
-            }
-            return primes;
-        }
-
-
-        private void bgWorkerDoWork_Compute(object sender, DoWorkEventArgs e)
-        {
-            BackgroundWorker worker = sender as BackgroundWorker;
-            int lBound = int.Parse(msktxtLowerBound.Text);
-            int uBound = int.Parse(msktxtUpperBound.Text);
-            //listResults = new List<ListViewItem>();
-            Console.WriteLine(lBound+ uBound);
-            hashSetPrimes = primes(uBound);
-            for(int i = lBound; i <= uBound; i++)
-            {
-                if(worker.CancellationPending== true)
+                if (worker.CancellationPending == true)
                 {
                     e.Cancel = true;
                     break;
                 }
                 else
                 {
-                    //System.Threading.Thread.Sleep(500);
+                    System.Threading.Thread.Sleep(1);
+                    bool flagP = true;
+                    int bound2 = (int)Math.Ceiling(Math.Sqrt(upperBound));
+                    for (int j = 2; j <= bound2; j++)
+                    {
+                        if (i % j == 0 && i != j)
+                        {
+                            flagP = false;
+                            break;
+                        }
+                    }
+                    if (flagP)
+                        primes.Add(i);
 
-                    listResults.Add(primes_compute(i, hashSetPrimes));
+                    //listResults.Add(primes_compute(i, hashSetPrimes));
+                    //ListViewItem factors = primes_compute(i, hashSetPrimes);
+                    worker.ReportProgress(i * 100 / bound1, "Computation1: Primes up to square root of upperbound!");
+                }
 
-                    //lblProgress.Text = ((i - lBound) * 100 / (uBound - lBound)).ToString();
-                    worker.ReportProgress((i-lBound) * 100/(uBound-lBound));
-                    
+
+            }
+            return primes;
+        }
+
+
+        /************************************************************************************
+         *  bgWorkerDoWork_Compute() : controlls the background thread computations & calls to report progress changes/results
+         * 
+         ***********************************************************************************/
+        private void bgWorkerDoWork_Compute(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            //Extract arguments sent from BGworker1.runsync()
+            int lBound = ((List<int>)e.Argument)[0];
+            int uBound = ((List<int>)e.Argument)[1];
+
+
+            if (lBound <= uBound)
+            {
+                Console.WriteLine(lBound + " - " + uBound);
+                hashSetPrimes = primes(uBound, worker, e);
+                Console.WriteLine("Computation1 Finished");
+
+                //sleep thread to allow time to update prograss bar1 completely
+                System.Threading.Thread.Sleep(1000);
+                //lblProgress.Text = "Computation1 Finished";
+
+                for (int i = lBound; i <= uBound; i++)
+                {
+                    if (worker.CancellationPending == true)
+                    {
+                        e.Cancel = true;
+                        break;
+                    }
+                    else if(lBound!=uBound)
+                    {
+                        System.Threading.Thread.Sleep(1);
+                        //listResults.Add(primes_compute(i, hashSetPrimes));
+                        ListViewItem factors = primes_compute(i, hashSetPrimes);
+                        worker.ReportProgress((i - lBound) * 100 / (uBound - lBound), factors);
+                    }
+                    else
+                    {
+                        //sleep thread to control refressh rate of UI update of factors on screen to prevent freeze of application
+                        System.Threading.Thread.Sleep(1);
+                        
+                        ListViewItem factors = primes_compute(i, hashSetPrimes);
+                        worker.ReportProgress(100, factors);
+                        
+                    }
                 }
             }
-        }
+            else
+            {
+                throw new Exception("Lowerbound value greater than Upperbound.");
+            }
 
-        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
-        {
-
-        }
-
-        private void backgroundWorker2_ProgressChanged(object sender, ProgressChangedEventArgs e)
-        {
 
         }
 
-        private void backgroundWorker2_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
-        {
-
-        }
     }
 }
