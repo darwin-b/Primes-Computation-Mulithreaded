0b780f599cd95dd9eb4d902499f9eef5260090bd
darwin_b <darwin.bollepalli@gmail.com>
2020-10-20 06:57:58 +0530

    Add project files.

---
+++ Primes-Computation-Mulithreaded/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Primes_Computation_Mulithreaded
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnCompute = new System.Windows.Forms.Button();
+            this.msktxtLowerBound = new System.Windows.Forms.MaskedTextBox();
+            this.msktxtUpperBound = new System.Windows.Forms.MaskedTextBox();
+            this.lblLowerBound = new System.Windows.Forms.Label();
+            this.lblUpperBound = new System.Windows.Forms.Label();
+            this.lstViewResults = new System.Windows.Forms.ListView();
+            this.colNumber = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colFactors = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblProgress);
+            this.panel1.Controls.Add(this.progressBar1);
+            this.panel1.Controls.Add(this.lblMsg);
+            this.panel1.Controls.Add(this.lstViewResults);
+            this.panel1.Controls.Add(this.btnCompute);
+            this.panel1.Controls.Add(this.msktxtLowerBound);
+            this.panel1.Controls.Add(this.msktxtUpperBound);
+            this.panel1.Controls.Add(this.lblLowerBound);
+            this.panel1.Controls.Add(this.lblUpperBound);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(612, 696);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnCompute
+            // 
+            this.btnCompute.Location = new System.Drawing.Point(36, 160);
+            this.btnCompute.Name = "btnCompute";
+            this.btnCompute.Size = new System.Drawing.Size(100, 40);
+            this.btnCompute.TabIndex = 6;
+            this.btnCompute.Text = "Compute";
+            this.btnCompute.UseVisualStyleBackColor = true;
+            this.btnCompute.Click += new System.EventHandler(this.btnCompute_Click);
+            // 
+            // msktxtLowerBound
+            // 
+            this.msktxtLowerBound.Location = new System.Drawing.Point(109, 109);
+            this.msktxtLowerBound.Mask = "000000000000000000";
+            this.msktxtLowerBound.Name = "msktxtLowerBound";
+            this.msktxtLowerBound.Size = new System.Drawing.Size(128, 20);
+            this.msktxtLowerBound.TabIndex = 5;
+            // 
+            // msktxtUpperBound
+            // 
+            this.msktxtUpperBound.Location = new System.Drawing.Point(109, 73);
+            this.msktxtUpperBound.Mask = "000000000000000000";
+            this.msktxtUpperBound.Name = "msktxtUpperBound";
+            this.msktxtUpperBound.Size = new System.Drawing.Size(128, 20);
+            this.msktxtUpperBound.TabIndex = 4;
+            // 
+            // lblLowerBound
+            // 
+            this.lblLowerBound.AutoSize = true;
+            this.lblLowerBound.Location = new System.Drawing.Point(33, 112);
+            this.lblLowerBound.Name = "lblLowerBound";
+            this.lblLowerBound.Size = new System.Drawing.Size(70, 13);
+            this.lblLowerBound.TabIndex = 1;
+            this.lblLowerBound.Text = "Lower Bound";
+            // 
+            // lblUpperBound
+            // 
+            this.lblUpperBound.AutoSize = true;
+            this.lblUpperBound.Location = new System.Drawing.Point(33, 76);
+            this.lblUpperBound.Name = "lblUpperBound";
+            this.lblUpperBound.Size = new System.Drawing.Size(70, 13);
+            this.lblUpperBound.TabIndex = 0;
+            this.lblUpperBound.Text = "Upper Bound";
+            // 
+            // lstViewResults
+            // 
+            this.lstViewResults.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colNumber,
+            this.colFactors});
+            this.lstViewResults.HideSelection = false;
+            this.lstViewResults.Location = new System.Drawing.Point(278, 73);
+            this.lstViewResults.Name = "lstViewResults";
+            this.lstViewResults.Size = new System.Drawing.Size(316, 562);
+            this.lstViewResults.TabIndex = 7;
+            this.lstViewResults.UseCompatibleStateImageBehavior = false;
+            this.lstViewResults.View = System.Windows.Forms.View.Details;
+            // 
+            // colNumber
+            // 
+            this.colNumber.Text = "Number";
+            this.colNumber.Width = 86;
+            // 
+            // colFactors
+            // 
+            this.colFactors.Text = "Factors";
+            this.colFactors.Width = 203;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgWorkerDoWork_Compute);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Location = new System.Drawing.Point(36, 258);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(35, 13);
+            this.lblMsg.TabIndex = 8;
+            this.lblMsg.Text = "status";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(36, 359);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.AutoSize = true;
+            this.lblProgress.Location = new System.Drawing.Point(33, 310);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(48, 13);
+            this.lblProgress.TabIndex = 10;
+            this.lblProgress.Text = "Progress";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 720);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MaskedTextBox msktxtLowerBound;
+        private System.Windows.Forms.MaskedTextBox msktxtUpperBound;
+        private System.Windows.Forms.Label lblLowerBound;
+        private System.Windows.Forms.Label lblUpperBound;
+        private System.Windows.Forms.Button btnCompute;
+        private System.Windows.Forms.ListView lstViewResults;
+        private System.Windows.Forms.ColumnHeader colNumber;
+        private System.Windows.Forms.ColumnHeader colFactors;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblProgress;
+    }
+}
+
---
+++ Primes-Computation-Mulithreaded/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Primes_Computation_Mulithreaded
+{
+    public partial class Form1 : Form
+    {
+
+        List<ListViewItem> listResults = new List<ListViewItem>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+            lstViewResults.Items.Add(primes_compute(10));
+        }
+
+        private void btnCompute_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.IsBusy && backgroundWorker1.WorkerSupportsCancellation == true)
+            {
+                lblMsg.Text = "Bg busy and cancelling";
+                listResults = new List<ListViewItem>();
+                lstViewResults.Items.Clear();
+                backgroundWorker1.CancelAsync();
+                //backgroundWorker1.RunWorkerAsync(1000);
+            }
+            else
+            {
+                lblMsg.Text = "BG not busy - starting worker";
+                lstViewResults.Items.Clear();
+                backgroundWorker1.CancelAsync();
+                lblProgress.Text = "";
+                progressBar1.Value = 0;
+                backgroundWorker1.RunWorkerAsync(1000);
+            }
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            backgroundWorker1.DoWork += bgWorkerDoWork_Compute;
+
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled) lblProgress.Text = "Operation was canceled";
+            else if (e.Error != null) lblProgress.Text = "Error: " + e.Error.Message;
+            else
+            {
+                lstViewResults.Items.Clear();
+                foreach (ListViewItem item in listResults)
+                {
+                    lstViewResults.Items.Add(item);
+                }
+                listResults = new List<ListViewItem>();
+            };
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            lblProgress.Text = (e.ProgressPercentage.ToString() + "%");
+            progressBar1.Value = int.Parse(e.ProgressPercentage.ToString());
+        }
+
+
+        private ListViewItem primes_compute(int num)
+        {
+            //lstViewResults.Items.Add(num.ToString());
+            ListViewItem number = new ListViewItem(num.ToString());
+            int bound = (int) Math.Sqrt(num);
+            string factors="";
+            for(int i=2; i<= num;i++)
+            {
+                if(num%i==0) factors += i.ToString()+" ";
+
+                while (num%i==0)
+                {
+                    num = num / i;
+                }
+            }
+
+            number.SubItems.Add(factors);
+            //lstViewResults.Items.Add(number);
+            return number;
+        }
+
+        private void bgWorkerDoWork_Compute(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            int lBound = int.Parse(msktxtLowerBound.Text);
+            int uBound = int.Parse(msktxtUpperBound.Text);
+            //listResults = new List<ListViewItem>();
+            Console.WriteLine(lBound+ uBound);
+            for(int i = lBound; i <= uBound; i++)
+            {
+                if(worker.CancellationPending== true)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    //System.Threading.Thread.Sleep(500);
+                    listResults.Add(primes_compute(i));
+
+                    //lblProgress.Text = ((i - lBound) * 100 / (uBound - lBound)).ToString();
+                    worker.ReportProgress((i-lBound) * 100/(uBound-lBound));
+                }
+            }
+        }
+    }
+}
